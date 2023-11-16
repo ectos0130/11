2599f297c330a1fd4cd7727ad43c6cda701d37c1
ectos0130 <ectos0130@naver.com>
2023-11-16 16:01:52 +0900

    05

--- main.c
+++ main.c
@@ -4,21 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	int grade[5];
-	int average = 0;
+	char*pc;
+	int*pi;
+	double*pd;
 	
-	for(i=0;i<5;i++){
-		printf("input vlaue[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	pc = (char*)10000;
+	pi = (int*)10000;
+	pd = (double*)10000;
 	
-	for(i=0;i<5;i++){
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		average = average + *(grade+i);
-	}
+	printf("중가전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
-	printf("average = %i\n", average/5);
+	pc++;
+	pi++;
+	pd++;
+	printf("증가후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
 	return 0;
 }
